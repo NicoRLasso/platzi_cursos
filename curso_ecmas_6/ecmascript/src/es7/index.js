cb1c64f45949fc234a454dec235c425b87a4cc88
NicoRLasso <nicolas.rlasso@gmail.com>
2022-04-19 12:01:55 -0500

    Feat: es7 version includes and pow operator

---
+++ curso_ecmas_6/ecmascript/src/es7/index.js
@@ -0,0 +1,14 @@
+// Includes array
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+  console.log("Yes, the number 7 is there ");
+} else {
+  console.log("No, is not there");
+}
+
+// Pow operator
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
